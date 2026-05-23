e1565010bd035d41f8626786359a7cc93974e2c2
myxa-biz-biz <kolupaevdenisss@gmail.com>
2026-05-23 17:33:57 +0300

    тут страницы и переход также шаблон описание игры

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+from flask import Flask, render_template, url_for
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('vstyplenie.html')
+
+@app.route('/osnova/')
+def osnova():
+	return render_template('osnovnay_chast.html')
+
+@app.route('/modi/')
+def modi():
+	return render_template('modi_i_resyrspaki.html')
+
+
+
+
+
+
+
+
+
+
+
+
+app.run()
